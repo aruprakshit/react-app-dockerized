cdb74ad45cac67639cc9a19ba6fa49541f3abe69
arup rakshit <aruprakshit@rocketmail.com>
2018-12-29 18:55:15 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# For production run the below steps:
+- `docker build .` from project root
+- docker run -p 8080:80 <image-id>
---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import Todo from './Todo';
+
+export default function App(props) {
+  return <Todo/>
+}
